4f9ed8370c54b79e0be95041ef1b56013e0e38de
Bruno Krebs <bruno.krebs@auth0.com>
2019-02-28 11:12:37 -0300

    ### Defining your first Django view

--- src/webapp/views.py
+++ src/webapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    products = [
+        {'title': 'PlayStation', 'price': 300, 'image': 'https://cdn.auth0.com/blog/django-webapp/playstation.png'},
+        {'title': 'iPhone', 'price': 900, 'image': 'https://cdn.auth0.com/blog/django-webapp/iphone.png'},
+        {'title': 'Yummy Pizza', 'price': 10, 'image': 'https://cdn.auth0.com/blog/django-webapp/pizza.png'},
+    ]
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'webapp/index.html', context)
