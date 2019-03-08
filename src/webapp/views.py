9a95a502e0cee461ec0ad616ec4e1b21fc7c8923
Bruno Krebs <bruno.krebs@auth0.com>
2019-03-08 18:44:19 -0300

    profile page;

--- src/webapp/views.py
+++ src/webapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+import json
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     products = [
@@ -12,3 +15,19 @@ def index(request):
         'products': products,
     }
     return render(request, 'webapp/index.html', context)
+
+
+@login_required
+def profile(request):
+    user = request.user
+    auth0user = user.social_auth.get(provider='auth0')
+    userdata = {
+        'user_id': auth0user.uid,
+        'name': user.first_name,
+        'picture': auth0user.extra_data['picture']
+    }
+
+    return render(request, 'webapp/profile.html', {
+        'auth0User': auth0user,
+        'userdata': json.dumps(userdata, indent=4)
+    })
