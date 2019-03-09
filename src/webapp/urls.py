e36307b9be1a21ac0e628b8e7f8a6b51f5e194e0
Bruno Krebs <bruno.krebs@auth0.com>
2019-03-09 17:15:19 -0300

    disabling the login screen;

--- src/webapp/urls.py
+++ src/webapp/urls.py
@@ -2,9 +2,15 @@ from django.urls import include, path
 
 from . import views
 
+from django.contrib.auth.decorators import login_required
+from django.contrib import admin
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('social_django.urls')),
     path('profile/', views.profile),
     path('logout/', views.logout),
 ]
+
+admin.autodiscover()
+admin.site.login = login_required(admin.site.login)
